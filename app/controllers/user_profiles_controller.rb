8c4ebc5a9300413b01c37a96fec8fd9a1242948f
Riya Raju <riya.raju@mobme.in>
2017-05-08 17:46:34 +0530

    Added add new user and edit options

--- app/controllers/user_profiles_controller.rb
+++ app/controllers/user_profiles_controller.rb
@@ -8,7 +8,12 @@ class UserProfilesController < ApplicationController
     end
   end
 
+  def new
+    @users = User.new
+  end
+
   def edit
+    @users = User.find(params[:id])
   end
 
   def list
@@ -19,6 +24,28 @@ class UserProfilesController < ApplicationController
     User.find(params[:id]).destroy
     flash[:success] = "User deleted"
     redirect_to :back
+  end
 
+  def create
+    @users = User.create!(params_user)
+    if @users.save
+      redirect_to user_profiles_list_path
+    else
+      p 'hiiiii'
+    end
+  end
+
+  def update
+    @users = User.find(params[:id])
+    if @users.update_attributes(params_user)
+      redirect_to user_profiles_list_path
+    end
+  end
+
+
+  private
+
+  def params_user
+    params.require(:user).permit(:email, :password, :password_confirmation, :username, :phone_number, :age)
   end
 end
