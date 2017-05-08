75927cbd63f1b81e122c4bc7c74bdb6754667984
Riya Raju <riya.raju@mobme.in>
2017-05-08 11:51:10 +0530

    User Profile View

--- app/controllers/user_profiles_controller.rb
+++ app/controllers/user_profiles_controller.rb
@@ -1,7 +1,11 @@
 class UserProfilesController < ApplicationController
   before_action :authenticate_user!
   def index
-
+    if params[:id]
+      @user = User.find(params[:id])
+    else
+      @user = current_user
+    end
   end
 
   def edit
