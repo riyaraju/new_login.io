87516ce5243a467a33724bbde5a74648b76a4ac0
Riya Raju <riya.raju@mobme.in>
2017-05-08 14:37:39 +0530

    phone number validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   validates :age, presence: true
   validates :phone_number,:presence => true,
             :numericality => true,
-            :length => { :minimum => 10, :maximum => 15 }
+            :length => { :minimum => 10, :maximum => 10 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
             format: { with: VALID_EMAIL_REGEX }
